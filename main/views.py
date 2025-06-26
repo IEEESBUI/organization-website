14957f89269457c0ec30e0ff76d346930f56c75d
fikaradrv <fikar2006@gmail.com>
2025-06-26 23:17:46 +0700

    ready deploy

--- main/views.py
+++ main/views.py
@@ -1,5 +1,14 @@
 # Tambahkan di views.py di aplikasi utama
 from django.shortcuts import render
+from event.models import Event
+from article.models import Article
+from django.utils import timezone
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    today = timezone.now().date()
+    events = Event.objects.filter(date__gte=today).order_by('date', 'time')[:3]
+    articles = Article.objects.filter(status='published').order_by('-created_at')[:3]
+    return render(request, 'homepage.html', {
+        'events': events,
+        'articles': articles,
+    })
