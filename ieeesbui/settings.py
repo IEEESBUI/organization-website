d2794af3c873771f6c6a592a44e3569bf9275180
fikaradrv <fikar2006@gmail.com>
2025-07-01 00:03:17 +0700

    fix: change static to link in division

--- ieeesbui/settings.py
+++ ieeesbui/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_-nuk1n!d+u+(316xj3slcgb^9hgmi5xre@!h^)vc1^16edzms'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', "ieeesbui.com", "*"]
 
